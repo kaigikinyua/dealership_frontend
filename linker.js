ae8cf25996b6dd46ef8d3a77dce09cd52547f547
kaigikinyua <kaigikinyua@gmail.com>
2018-12-21 01:38:37 +0300

    ran into security issues with electron

---
+++ linker.js
@@ -0,0 +1,18 @@
+console.log("Hello From linker.js");
+function testP(){
+	//var python=require("python-shell");
+	//var path=require("path");
+
+	//var name=document.getElementById("uname").value
+	console.log("Hae");
+
+	/*var options={
+		scriptPath:path.join(__dirname+'./');
+		args:[name];
+	}
+
+	var response=new python('python.py',options);
+	response.on('message',function(message){
+		swal(message);
+	})*/
+}
