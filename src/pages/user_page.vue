65dc1cab2cb150d9b08d5d22ac9c51d4b120d475
kaigikinyua <kaigikinyua@gmail.com>
2020-07-12 15:14:38 +0300

    created userpage view - authentication not done

---
+++ src/pages/user_page.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="userpage">
+        <topnav v-bind:barner="barner" v-bind:links="links"></topnav>
+    </div>
+</template>
+<script>
+import Topnav from './components/topnav'
+export default {
+    components:{
+        "topnav":Topnav
+    },
+    data(){
+        return {
+            barner:{"title":"Virtual Dealership","subtitle":"Username"},
+            links:[{"title":"Logout","link":"/"}]
+        }
+    },
+    methods:{
+        fetch_cars:function(){}
+    }    
+}
+</script>
+<style scoped>
+
+</style>
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,11 @@
+import Index from './pages/index'
 import Login from './pages/login'
 import Signup from './pages/signup'
-import Index from './pages/index'
+import UserPage from './pages/user_page.vue'
 
 export default[
     {'path':'/',component:Index},
     {'path':'/login',component:Login},
     {'path':'/signup',component:Signup},
-
+    {'path':'/user_page','component':UserPage}
 ]
